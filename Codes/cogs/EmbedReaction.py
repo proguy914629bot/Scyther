fe7deb199e9da86689a1b653c670022e7275d8dc
proguy914629bot <74696067+proguy914629bot@users.noreply.github.com>
2020-11-21 11:37:28 +0700

    Create EmbedReaction.py

---
+++ Codes/cogs/EmbedReaction.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import DiscordUtils
+
+class EmbedWithReactions(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command()
+    async def EmbedWithReaction(self, ctx):
+        embed1 = discord.Embed(color=ctx.author.color).add_field(name="Example1", value="Page 1").add_field(name = 'Example2', value = 'Page 1')
+        embed2 = discord.Embed(color=ctx.author.color).add_field(name="Example1", value="Page 2").add_field(name = 'Example2', value = 'Page 2').add_field(name = 'Example3', value = 'Page 1')
+        embed3 = discord.Embed(color=ctx.author.color).add_field(name="Example1", value="Page 3")
+        paginator = DiscordUtils.Pagination.CustomEmbedPaginator(ctx)
+        paginator.add_reaction('⏮️', "first")
+        paginator.add_reaction('⏪', "back")
+        paginator.add_reaction('🔐', "lock")
+        paginator.add_reaction('⏩', "next")
+        paginator.add_reaction('⏭️', "last")
+        embeds = [embed1, embed2, embed3]
+        await paginator.run(embeds)
+        
+def setup(bot):
+    bot.add_cog(EmbedWithReactions(bot))
+    print('EmbedWithReaction Command Cog is Ready!')
